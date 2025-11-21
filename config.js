05b790ff1d197d69e8cebe6cab100b201cd10bef
alltarego <vitor.altarego@gmail.com>
2025-11-21 15:43:19 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+
+var config = {
+    
+    lines: [
+        "Meu amor",
+        "Cada momento ao seu lado",
+        "É uma lembrança que guardo no coração",
+        "Seu sorriso ilumina meus dias",
+        "E cada dia que passa",
+        "Meu amor por você só cresce",
+        "Te amo infinitamente...",
+    ],
+ 
+    date: "2025-05-23T23:36:00",
+    
+    names: ["Vitor", "Camila"]
+}
